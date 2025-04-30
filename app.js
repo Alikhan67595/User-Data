de689139005ee0753fbe4260ba0ded2d5e5b997e
Alikhan67595 <hanbal22hassan@gmail.com>
2025-05-01 03:21:00 +0500

    User Data

--- app.js
+++ app.js
@@ -169,4 +169,374 @@ var data = [
       },
       phone: "1-104-001-4567",
     },
-  ];
+    {
+      address: {
+        city: "austin",
+        street: "pine ridge dr",
+        number: 4532,
+        zipcode: "78901-2345",
+      },
+      id: 11,
+      email: "emma@gmail.com",
+      username: "emma_w",
+      password: "em123!@#",
+      name: {
+        firstname: "emma",
+        lastname: "watson",
+      },
+      phone: "1-512-789-4567",
+    },
+    {
+      address: {
+        city: "denver",
+        street: "maple ave",
+        number: 987,
+        zipcode: "80234-5678",
+      },
+      id: 12,
+      email: "liam@gmail.com",
+      username: "liam_n",
+      password: "n345$%^",
+      name: {
+        firstname: "liam",
+        lastname: "norris",
+      },
+      phone: "1-303-456-7890",
+    },
+    {
+      address: {
+        city: "seattle",
+        street: "cedar ln",
+        number: 3214,
+        zipcode: "98123-4567",
+      },
+      id: 13,
+      email: "olivia@gmail.com",
+      username: "olivia_m",
+      password: "oli#456*",
+      name: {
+        firstname: "olivia",
+        lastname: "mason",
+      },
+      phone: "1-206-789-1234",
+    },
+    {
+      address: {
+        city: "boston",
+        street: "harbor view dr",
+        number: 654,
+        zipcode: "02134-6789",
+      },
+      id: 14,
+      email: "noah@gmail.com",
+      username: "noah_b",
+      password: "no789^&*",
+      name: {
+        firstname: "noah",
+        lastname: "brooks",
+      },
+      phone: "1-617-234-5678",
+    },
+    {
+      address: {
+        city: "chicago",
+        street: "lake shore dr",
+        number: 8765,
+        zipcode: "60601-2345",
+      },
+      id: 15,
+      email: "ava@gmail.com",
+      username: "ava_s",
+      password: "ava!234#",
+      name: {
+        firstname: "ava",
+        lastname: "scott",
+      },
+      phone: "1-312-678-9012",
+    },
+    {
+      address: {
+        city: "houston",
+        street: "westheimer rd",
+        number: 543,
+        zipcode: "77056-7890",
+      },
+      id: 16,
+      email: "ethan@gmail.com",
+      username: "ethan_h",
+      password: "eth456$@",
+      name: {
+        firstname: "ethan",
+        lastname: "harris",
+      },
+      phone: "1-713-123-4567",
+    },
+    {
+      address: {
+        city: "phoenix",
+        street: "camelback rd",
+        number: 2345,
+        zipcode: "85016-3456",
+      },
+      id: 17,
+      email: "sophia@gmail.com",
+      username: "sophia_l",
+      password: "sop789*%",
+      name: {
+        firstname: "sophia",
+        lastname: "lewis",
+      },
+      phone: "1-602-789-2345",
+    },
+    {
+      address: {
+        city: "los angeles",
+        street: "sunset blvd",
+        number: 7890,
+        zipcode: "90028-5678",
+      },
+      id: 18,
+      email: "mason@gmail.com",
+      username: "mason_c",
+      password: "mas123!^",
+      name: {
+        firstname: "mason",
+        lastname: "clark",
+      },
+      phone: "1-323-456-7890",
+    },
+    {
+      address: {
+        city: "atlanta",
+        street: "peachtree st",
+        number: 1234,
+        zipcode: "30309-2345",
+      },
+      id: 19,
+      email: "isabella@gmail.com",
+      username: "isabella_r",
+      password: "isa456#@",
+      name: {
+        firstname: "isabella",
+        lastname: "reed",
+      },
+      phone: "1-404-789-3456",
+    },
+    {
+      address: {
+        city: "dallas",
+        street: "main st",
+        number: 5678,
+        zipcode: "75201-6789",
+      },
+      id: 20,
+      email: "jacob@gmail.com",
+      username: "jacob_m",
+      password: "jac789$%",
+      name: {
+        firstname: "jacob",
+        lastname: "murphy",
+      },
+      phone: "1-214-123-7890",
+    },
+    {
+      address: {
+        city: "orlando",
+        street: "orange ave",
+        number: 4321,
+        zipcode: "32801-3456",
+      },
+      id: 21,
+      email: "mia@gmail.com",
+      username: "mia_t",
+      password: "mia123^&",
+      name: {
+        firstname: "mia",
+        lastname: "turner",
+      },
+      phone: "1-407-456-1234",
+    },
+    {
+      address: {
+        city: "portland",
+        street: "burnside st",
+        number: 9876,
+        zipcode: "97209-5678",
+      },
+      id: 22,
+      email: "logan@gmail.com",
+      username: "logan_w",
+      password: "log456!@",
+      name: {
+        firstname: "logan",
+        lastname: "walker",
+      },
+      phone: "1-503-789-4567",
+    },
+    {
+      address: {
+        city: "nashville",
+        street: "broadway",
+        number: 3456,
+        zipcode: "37203-2345",
+      },
+      id: 23,
+      email: "harper@gmail.com",
+      username: "harper_b",
+      password: "har789#%",
+      name: {
+        firstname: "harper",
+        lastname: "bailey",
+      },
+      phone: "1-615-123-5678",
+    },
+    {
+      address: {
+        city: "las vegas",
+        street: "las vegas blvd",
+        number: 6789,
+        zipcode: "89109-6789",
+      },
+      id: 24,
+      email: "elijah@gmail.com",
+      username: "elijah_c",
+      password: "eli123$@",
+      name: {
+        firstname: "elijah",
+        lastname: "cooper",
+      },
+      phone: "1-702-456-7890",
+    },
+    {
+      address: {
+        city: "charlotte",
+        street: "tryon st",
+        number: 2345,
+        zipcode: "28202-3456",
+      },
+      id: 25,
+      email: "amelia@gmail.com",
+      username: "amelia_p",
+      password: "ame456!^",
+      name: {
+        firstname: "amelia",
+        lastname: "parker",
+      },
+      phone: "1-704-789-1234",
+    },
+    {
+      address: {
+        city: "raleigh",
+        street: "fayetteville st",
+        number: 5432,
+        zipcode: "27601-5678",
+      },
+      id: 26,
+      email: "benjamin@gmail.com",
+      username: "benjamin_m",
+      password: "ben789#@",
+      name: {
+        firstname: "benjamin",
+        lastname: "mitchell",
+      },
+      phone: "1-919-123-4567",
+    },
+    {
+      address: {
+        city: "minneapolis",
+        street: "hennepin ave",
+        number: 8765,
+        zipcode: "55401-2345",
+      },
+      id: 27,
+      email: "charlotte@gmail.com",
+      username: "charlotte_w",
+      password: "cha123$%",
+      name: {
+        firstname: "charlotte",
+        lastname: "white",
+      },
+      phone: "1-612-789-5678",
+    },
+    {
+      address: {
+        city: "tampa",
+        street: "bayshore blvd",
+        number: 3214,
+        zipcode: "33606-6789",
+      },
+      id: 28,
+      email: "daniel@gmail.com",
+      username: "daniel_r",
+      password: "dan456!@",
+      name: {
+        firstname: "daniel",
+        lastname: "roberts",
+      },
+      phone: "1-813-123-7890",
+    },
+    {
+      address: {
+        city: "san diego",
+        street: "la jolla blvd",
+        number: 6543,
+        zipcode: "92037-3456",
+      },
+      id: 29,
+      email: "evelyn@gmail.com",
+      username: "evelyn_t",
+      password: "eve789^&",
+      name: {
+        firstname: "evelyn",
+        lastname: "thomas",
+      },
+      phone: "1-619-456-1234",
+    },
+    {
+      address: {
+        city: "salt lake city",
+        street: "state st",
+        number: 9876,
+        zipcode: "84111-5678",
+      },
+      id: 30,
+      email: "henry@gmail.com",
+      username: "henry_b",
+      password: "hen123#@",
+      name: {
+        firstname: "henry",
+        lastname: "baker",
+      },
+      phone: "1-801-789-4567",
+    },
+];
+
+  var main = document.querySelector('#main')
+  var mainbgColor = document.querySelector('#bgColor')
+
+  // div close function
+  function closeDiv(){
+    mainbgColor.style.display = 'none'
+  }
+
+  for (var i=0 ; i < data.length ; i++){
+
+    var card = document.createElement("div")
+    card.className = 'card'
+    
+    for (var key in data[i].name){ // object mai sai name extract karta hai
+        if (key === 'firstname'){
+        var mainName = data[i].name[key]
+        card.innerText = mainName.toUpperCase()
+        }}//
+
+        card.addEventListener('click', function(e){
+
+            mainbgColor.style.display = 'flex'
+        }
+    )
+
+    
+
+    main.appendChild(card)
+  }
